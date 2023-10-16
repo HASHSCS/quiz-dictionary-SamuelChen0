97a81e2e337b380abbebbf07a5fc529f8cc99293
SamuelChen0 <chenpinrong0808@gmail.com>
2023-10-16 11:20:59 +0800

    g

--- solutions/dicts.py
+++ solutions/dicts.py
@@ -3,7 +3,8 @@ def get_value(data_dict, key):
     Returns the value for the given key in the dictionary.
     If the key is not in the dictionary, return None.
     """
-    pass #Replace with your code
+    data=data_dict.get(key)
+    return data
 
 
 def remove_key(data_dict, key):
@@ -12,4 +13,4 @@ def remove_key(data_dict, key):
     If the key is not found, the dictionary should remain unchanged.
     Returns the modified dictionary.
     """
-    pass #Replace with your code
+
